408d38c9fa070ed101929bcd2d0f234c8acbc893
Jesse A Janowiak <jesse@insidenewcity.com>
2020-05-23 23:03:02 -0400

    Non-customizable glyph options all working

--- src/plugins/spacer-glyphs.js
+++ src/plugins/spacer-glyphs.js
@@ -4,10 +4,21 @@ const _ = require('lodash');
 const spacerPrefixes = (customPrefixes = []) => {
   return _.uniq([
     ...customPrefixes,
-    'spacer-glyph-x'
+    'spacer-glyph'
   ]);
 }
 
+
+// TO-DO: Write tests passing types of glyph values:
+// - string
+// - object with default and reverse
+// - object with default and no reverse
+// - object with reverse and no default
+// - object with extra invalid keys
+// - object with only invalid keys
+// - not object or string
+
+// TO-DO: Accept glyphs from user theme
 const spacerGlyphs = (customGlyphs = {}) => {
   const defaultGlyphs = {
     'angle': {
@@ -25,64 +36,90 @@ const spacerGlyphs = (customGlyphs = {}) => {
     'slash-double': '//',
   };
 
-  return Object.assign({}, customGlyphs, defaultGlyphs);
+  let combinedGlyphs = Object.assign({}, customGlyphs, defaultGlyphs)
+
+  let combinedGlyphsPairs = Object.entries(combinedGlyphs);
+
+  combinedGlyphsPairs = combinedGlyphsPairs.map((value) => {
+    if (typeof value[1] === 'string') {
+      return [
+        value[0],
+        {
+          'default': value[1],
+          'reverse': value[1]
+        }
+      ]
+    }
+    return value;
+  });
+  return Object.fromEntries(combinedGlyphsPairs);
+
 }
 
+
+// TO-DO: Enable custom font definition for glyphs
+// TO-DO: Enable variants
 module.exports = plugin(function ({
   theme,
   variants,
   e,
   addUtilities
 }) {
-  let spacerGlyphClasses = {
-    // '.spacer-glyph-x': {
+  let glyphWidth = theme('spacing')['2'];
+  let spacerGlyphClasses = {};
 
-    //   '>*': {
-    //     color: 'red',
-    //     backgroundColor: 'orange',
-    //   },
-    //   '>*::before': {
-    //     display: 'block',
-    //     position: 'absolute',
-    //     width: theme('spacing')['2'],
-    //     textAlign: 'center',
-    //     content: `'•'`
-    //   }
-    // },
+  let spacerGlyphDefaultProps = {
+    display: 'block',
+    position: 'absolute',
+    width: glyphWidth,
+    marginLeft: `calc(${glyphWidth} / -2)`,
+    textAlign: 'center',
+    content: `''`,
   };
 
+  // Generic .spacer-glyph-x with no additional modifiers
   _.forEach(spacerPrefixes(), (prefix) => {
     spacerGlyphClasses[`.${e(`${prefix}`)}`] = {
-      '&>*:not(:first-child)::before': {
-        display: 'block',
-        position: 'absolute',
-        width: theme('spacing')['2'],
-        textAlign: 'center',
-        content: `'•'`
-      }
-    };
+      '&>*::before': spacerGlyphDefaultProps,
+
+      // Default glyph: disc
+      '&:not(.space-x-reverse)>*:not(:first-child)::before': {
+        content: `'${spacerGlyphs()['disc'].default}'`
+      },
+      '&.space-x-reverse>*:not(:last-child)::before': {
+        content: `'${spacerGlyphs()['disc'].default}'`
+      },
+    }
+
+    // Spacer positioning for specific space widths (e.g. `.space-x-4`)
     _.forEach(theme('spacing'), (spacingAmt, spacingKey) => {
       spacerGlyphClasses[`.${e(`${prefix}`)}.space-x-${e(`${spacingKey}`)}`] = {
         '&>*::before': {
-          marginLeft: `calc(${spacingAmt} / -2 - 0.25rem)`,
+          marginLeft: `calc(${spacingAmt} / -2 - ${glyphWidth} / 2)`,
         }
       };
+    });
 
+    // Classes for choosing glyph
+    _.forEach(spacerGlyphs(), (glyph, glyphKey) => {
+      spacerGlyphClasses[`.${e(`${prefix}`)}-${glyphKey}`] = {
+        '&:not(.space-x-reverse)>*:not(:first-child)::before': {
+          content: `'${glyph.default}'`
+        },
+        '&.space-x-reverse>*:not(:last-child)::before': {
+          content: `'${glyph.reverse}'`
+        },
+      }
     });
   });
-  // const spacerGlyphClasses = {
-  //   '.spacer-x': {
-  //     color: 'blue',
-  //     fontWeight: 'bold',
-  //   }
-  // }
 
   addUtilities(spacerGlyphClasses);
+
 }, {
   theme: {
     spacerGlyphs: {},
   },
   variants: {
-    aspectRatio: ['responsive'],
+    spacerGlyphs: ['responsive'],
   },
 });
